45f4fec76199f873dc5ffef68ecf957dc64df4e2
Nabby <trishasalubreaquino@gmail.com>
2024-11-20 18:19:52 -0700

    updated the exceptions

--- src/exceptions/EmptyQueueException.java
+++ src/exceptions/EmptyQueueException.java
@@ -4,13 +4,12 @@ package exceptions;
  * Exception thrown when attempting to access an element from an empty queue.
  */
 public class EmptyQueueException extends Exception {
-    // Default constructor
-    public EmptyQueueException() {
-        super("The queue is empty.");
-    }
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 4841203106897644399L;
 
-    // Constructor that allows for a custom message
-    public EmptyQueueException(String message) {
+	public EmptyQueueException(String message) {
         super(message);
     }
 }
